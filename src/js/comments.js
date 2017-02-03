52f62edb36b44b07eb3525da10ffb49bcebdb47d
Sergio Alonso <sergio@sergioalonso.es>
2017-02-03 16:25:55 +0100

    Remove console logs

--- src/js/comments.js
+++ src/js/comments.js
@@ -2,14 +2,12 @@ var url = "http://" + location.host + "/db";
 var comments = $('<ul/>', {class: "comments"});
 
 function loadComment(i, item) {
-  console.log(item.comment);
   var comment = $('<li/>',{class: 'comment', text: item.comment}).appendTo(comments);
 }
 
 function loadComments(data) {
   $.each(data.comments, loadComment);
   $('#detail-comments').append(comments);
-  console.log($('#detail-comments'));
 }
 
 function loadData() {

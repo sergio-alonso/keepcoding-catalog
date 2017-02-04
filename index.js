455851e74aa66c2c4280e1cc78063f4917d227aa
Sergio Alonso <sergio@sergioalonso.es>
2017-02-04 18:58:21 +0100

    Make json-server work with browser-sync

---
+++ index.js
@@ -0,0 +1,17 @@
+var bs = require('browser-sync').create();
+var jsonServer = require('json-server');
+
+var server = jsonServer.create();
+server.use(jsonServer.defaults());
+server.use(jsonServer.router('src/db.json'));
+
+bs.init({
+    ui: false,
+    files: './public',
+    open: false,
+    server: {
+        baseDir: './public'
+    }
+}, function (err, bs) {
+    bs.app.use('/api', server);
+});

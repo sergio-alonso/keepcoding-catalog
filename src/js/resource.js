258b5945951e2319ed647f43778c332932391e18
Sergio Alonso <sergio@sergioalonso.es>
2017-02-06 01:25:17 +0100

    Sort functions to be enabled on load

--- src/js/resource.js
+++ src/js/resource.js
@@ -1,3 +1,46 @@
+var api = "http://" + location.host + "/api";
+var url = api + "/comments";
+var comments = $('<ul/>', {class: "comments"});
+
+function loadComment(i, item) {
+  var comment = $('<li/>',{class: 'comment', text: item.comment}).appendTo(comments);
+}
+
+function loadComments(data) {
+  $.each(data, loadComment);
+  $('#detail-comments').append(comments);
+}
+
+function loadData() {
+  $.getJSON(url, loadComments);
+}
+
+
+function saveComment(comment) {
+  jQuery.ajax({
+    type: "POST",
+    url: url,
+    dataType: 'json',
+    data: comment,
+    success: function (data) {
+      alert("Comment saved");
+    }
+  });
+}
+
+function onSubmit(event) {
+  var form = jQuery(event.target);
+  if (form.is("#comment-form")) {
+    event.preventDefault();
+    saveComment(form.serialize());
+  }
+}
+
+
+function saveData() {
+  jQuery(document).submit(onSubmit);
+}
+
 function showResource(event) {
     $("#detail-view").modal({
       remote: 'detail.html'

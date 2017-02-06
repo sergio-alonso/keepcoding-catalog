a0c3caa04d566d430ba6113f49ccfdc912ae72a5
Sergio Alonso <sergio@sergioalonso.es>
2017-02-06 01:48:56 +0100

    Count number of likes

--- src/js/like-it.js
+++ src/js/like-it.js
@@ -1,11 +1,11 @@
 function incrementLike(id) {
   var count = localStorage.getItem(id);
-  localStorage.setItem(id, count++);
+  count++;
+  localStorage.setItem(id, count);
 }
 
 $(".pin-icon").click(function (e) {
   e.stopPropagation();
   // console.log($(this).attr("resource-id"));
   incrementLike($(this).attr("resource-id"));
-//  removeFromView();
 });

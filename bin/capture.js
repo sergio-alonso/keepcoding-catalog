36c7c67692f1251bacbe6dc46dd352ddeac5b466
Sergio Alonso <sergio@sergioalonso.es>
2017-02-17 17:58:12 +0100

    Capture two pages is not working

--- bin/capture.js
+++ bin/capture.js
@@ -1,7 +1,15 @@
 var page = require('webpage').create();
 page.viewportSize = { width: 1280, height: 1024 };
 page.open('http://localhost:3000/page-landing.html', function() {
-  page.render('public/page-landing.png');
+  page.render('public/page-landing-lg.png');
   phantom.exit();
 });
 
+/*
+var page_landing_xs = require('webpage').create();
+page_landing_xs.viewportSize = { width: 375, height: 667 };
+page_landing_xs.open('http://localhost:3000/page-landing.html', function() {
+  page_landing_xs.render('public/page-landing-xs.png');
+  phantom.exit();
+});
+*/

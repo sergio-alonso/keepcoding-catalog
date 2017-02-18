7d1bf48e18f4b9f68e974d3ee01a22391448c31f
Sergio Alonso <sergio@sergioalonso.es>
2017-02-19 00:37:29 +0100

    Revert "Add script to capture screen using phantomjs"
    This reverts commit 97274d7d6a441f5561db1ff1dd5e688c8029cdda.

--- bin/capture.js
+++
@@ -1,7 +0,0 @@
-var page = require('webpage').create();
-page.viewportSize = { width: 1280, height: 1024 };
-page.open('http://localhost:3000/page-landing.html', function() {
-  page.render('public/page-landing.png');
-  phantom.exit();
-});
-

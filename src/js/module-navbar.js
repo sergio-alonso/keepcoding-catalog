01edf76d853e9e97a28b6cc825ded7ce834e2c9d
Sergio Alonso <sergio@sergioalonso.es>
2017-03-03 02:31:17 +0100

    Several changes, I will not do it again...

--- src/js/module-navbar.js
+++ src/js/module-navbar.js
@@ -1,55 +1,92 @@
 function moduleNavbar( sandbox ) {
   "use strict";
 
-  var _joinButton,
+  var _log = "module::navbar::",
+      _joinButton,
+      _joinButtonIcon,
       _loginButton,
       _loginButtonIcon;
 
-  function onInit() {
-    sandbox.log.debug( "module::navbar::onInit()" );
+  var onInit = function() {
+    sandbox.log.debug( _log + "onInit()" );
 
     _joinButton = sandbox.find( "#register" );
     _joinButton.bind( "click", _onJoinButtonClick );
+
+    _joinButtonIcon = sandbox.find( "#register > span.glyphicon" );
+    _setJoinButtonStyle();
 
     _loginButton = sandbox.find( "#login" );
     _loginButton.bind( "click", _onLoginButtonClick );
 
     _loginButtonIcon = sandbox.find( "#login > span.glyphicon" );
+    _setLoginButtonStyle();
 
     sandbox.subscribe( "msg-join-submit", _onJoinSubmitMessage );
     sandbox.subscribe( "msg-login-submit", _onLoginSubmitMessage );
-  }
+  };
 
-  function onDestroy() {
-    sandbox.log.debug( "module::navbar::onDestroy()" );
-  }
+  var onDestroy = function() {
+    sandbox.log.debug( _log + "onDestroy()" );
+  };
 
-  function _onJoinButtonClick() {
-    sandbox.log.debug( "module::navbar::_onJoinButtonClick()" );
+  var _onJoinButtonClick = function() {
+    sandbox.log.debug( _log + "_onJoinButtonClick()" );
 
-    sandbox.notify( "msg-join-request" );
-  }
+    if ( sandbox.isUserJoined() ) {
+      _logout();
+      sandbox.removeUserAccount();
+      _setJoinButtonStyle();
+    } else {
+      sandbox.notify( "msg-join-request" );
+    }
+  };
 
-  function _onLoginButtonClick() {
-    sandbox.log.debug( "module::navbar::_onLoginButtonClick()" );
-  }
+  var _onLoginButtonClick = function() {
+    sandbox.log.debug( _log + "_onLoginButtonClick()" );
 
-  function _onJoinSubmitMessage( data ) {
-    sandbox.log.debug( "module::navbar::_onJoinSubmitMessage()", data );
+    if ( sandbox.isUserLoged() ) {
+      _logout();
+    } else if ( sandbox.isUserJoined() ) {
+      sandbox.notify( "msg-login-request" );
+    } else {
+      sandbox.notify( "msg-join-request" );
+    }
+  };
+
+  var _onJoinSubmitMessage = function( data ) {
+    sandbox.log.debug( _log + "_onJoinSubmitMessage()", data );
 
     sandbox.store( "user", data );
 
-    _joinButton.remove();
-    sandbox.notify( "msg-login-submit", data );
-  }
+    _setJoinButtonStyle();
 
-  function _onLoginSubmitMessage( data ) {
-    sandbox.log.debug( "module::navbar::_onLoginSubmitMessage()", data );
+    sandbox.notify( "msg-login-submit", data );
+  };
+
+  var _onLoginSubmitMessage = function( data ) {
+    sandbox.log.debug( _log + "_onLoginSubmitMessage()", data );
 
     sandbox.session( "user", data );
 
-    _loginButtonIcon.toggleClass( "glyphicon-log-out" );
-  }
+    _setLoginButtonStyle();
+  };
+
+  var _logout = function() {
+    sandbox.log.debug( _log + "_onLogout()" );
+
+    sandbox.removeUserSession();
+
+    _setLoginButtonStyle();
+  };
+
+  var _setLoginButtonStyle = function() {
+    _loginButtonIcon.toggleClass( "glyphicon-log-out", sandbox.isUserLoged() );
+  };
+
+  var _setJoinButtonStyle = function() {
+    _joinButtonIcon.toggleClass( "glyphicon-minus", sandbox.isUserJoined() );
+  };
 
   return {
     init: onInit,

beb693ec4aedec0f5b0f09670a01b48444c2ed4c
Sergio Alonso <sergio@sergioalonso.es>
2017-03-04 23:17:50 +0100

    Send show article message

--- src/js/module-article.js
+++ src/js/module-article.js
@@ -2,25 +2,34 @@ function moduleArticle( sandbox ) {
   "use strict";
 
   var _log = "module::article::",
+      _container,
       _pinIcon;
 
   var onInit = function() {
     sandbox.log.debug( _log + "oninit()" );
 
+    _container = sandbox.find( ".resource" );
+    _container.bind( "click", onContainerClick );
+
     _pinIcon = sandbox.find( ".pin-icon" );
-    _pinIcon.bind( "click", onClick );
+    _pinIcon.bind( "click", onPinIconClick );
   };
 
   var onDestroy = function() {
     sandbox.log.debug( _log + "onDestroy()" );
   };
 
-  var onClick = function( event ) {
-    sandbox.log.warn( _log + "onClick() check if user is loged in" );
+  var onPinIconClick = function( event ) {
+    sandbox.log.warn( _log + "onPinIconClick() check if user is loged in" );
     var self = sandbox.find( this ).attr( "resource-id" );
     event.stopPropagation();
     sandbox.like( self );
     sandbox.notify( "msg-like-article", { id: self } );
+  };
+
+  var onContainerClick = function() {
+    sandbox.log.debug( _log + "onContainerClick()" );
+    sandbox.notify( "msg-show-article" );
   };
 
   return {
@@ -31,4 +40,3 @@ function moduleArticle( sandbox ) {
 }
 
 module.exports = moduleArticle;
-

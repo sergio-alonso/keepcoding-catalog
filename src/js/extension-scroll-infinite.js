7d563c0d8daa77390a50a27807ed099d0b5730a9
Sergio Alonso <sergio@sergioalonso.es>
2017-03-04 01:16:59 +0100

    Support infinite scroll

--- src/js/extension-scroll-infinite.js
+++ src/js/extension-scroll-infinite.js
@@ -9,18 +9,27 @@ function extensionScrollInfinite( core ) {
     core.log.debug( _log + "onInit()" );
 
     _doc = core.dom.find( document );
+
+    core.log.warn( _log + "onInit() link scroll to window instead of module?" );
     _window = core.dom.find( window );
-    _window.scroll( _infinite );
+
+    core.scroll = this;
   };
 
   var onDestroy = function() {
     core.log.debug( _log + "onDestroy()" );
   };
 
-  var _infinite = function() {
+  var _infinite = function( callback ) {
     if ( _doc.height() - _window.height() == _window.scrollTop() ) {
-      core.log.warn( "call ajax" );
+      callback();
     }
+  };
+
+  core.sandbox.infiniteScroll = function( callback ) {
+    _window.scroll( function() {
+      _infinite( callback );
+    } );
   };
 
   return {

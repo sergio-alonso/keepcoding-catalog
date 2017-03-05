f13c61fe4e453fc0f1ed47c53c0fc1b0a298d5ff
Sergio Alonso <sergio@sergioalonso.es>
2017-03-05 23:42:55 +0100

    doc: add comment

---
+++ src/js/framework-module.js
@@ -0,0 +1,23 @@
+/*
+ * Scalable JavaScript Application Architecture by Nicholas C.Zakas
+ *
+ */
+
+/*
+ * Module
+ *
+ * Independent unit of functionality that is part of the total
+ * structure of a web application.
+ *
+ */
+
+/*
+ * Each module has its own sandbox.
+ * An interface with which the module can interact to ensue loose coupling.
+ *
+ * Modules have limited knowledge.
+ * Each module knows about their sandbox and that's it.
+ *
+ * Each module's job is to create a meaningful user experience.
+ *
+ */

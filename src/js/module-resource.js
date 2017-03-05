12507c7f9b0a8d8efe50e147a87985a7c7c1cd14
Sergio Alonso <sergio@sergioalonso.es>
2017-03-05 02:45:11 +0100

    Support save comment

--- src/js/module-resource.js
+++ src/js/module-resource.js
@@ -4,16 +4,27 @@ function moduleResource( sandbox ) {
   var _log = "module::resource::",
       _api = "http://" + location.host + "/api",
       _url = _api + "/comments",
-      items = $( "<ul/>", { class: "items" } );
+      items = $( "<ul/>", { class: "items" } ),
+      form;
 
   var onInit = function() {
     sandbox.log.debug( _log + "onInit()" );
 
     sandbox.subscribe( "msg-showed-article", onShowArticle );
+
+    sandbox.find( document ).submit( onSubmit );
   };
 
   var onDestroy = function() {
     sandbox.log.debug( _log + "onDestroy()" );
+  };
+
+  var onSubmit = function( e ) {
+    form = sandbox.find( e.target );
+    if ( form.is( "#comment-form"  ) ) {
+      e.preventDefault();
+      sandbox.saveData( _url, form.serialize() );
+    }
   };
 
   var loadItem = function( i, item ) {
